3360f51f7d22bc94905191c9a3d308cdfdb02b67
Bruno Santos <brunocobal@hotmail.com>
2021-06-30 18:51:27 -0300

    Final Version

--- src/test/java/com/restassured/test/TestExamples.java
+++ src/test/java/com/restassured/test/TestExamples.java
@@ -1,31 +1,24 @@
-package tests;
+package com.restassured.test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.sun.xml.bind.v2.schemagen.xmlschema.List;
-
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-import static org.testng.Assert.*;
-
-import java.net.URI;
-
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.specification.RequestSpecification;
 
 public class TestExamples {
 
 		String baseURI = "http://localhost:3000";
 		
-//		@Test
-	public void test1() {
+		//That's a simples code that use my local API to do some com.restassured.test
+		
+		@Test
+	public void assertGetAll() {
 
 		Response response = RestAssured.get("http://localhost:3000/posts");
 		int statusCode = response.getStatusCode();
@@ -36,8 +29,8 @@ public class TestExamples {
 		Assert.assertEquals(statusCode,200);
 	}
 
-//		@Test
-	public void test2(){
+		@Test
+	public void assertGet(){
 
 		Response response = RestAssured.get("http://localhost:3000");
 		int statusCode = response.getStatusCode();
@@ -56,8 +49,8 @@ public class TestExamples {
 		equalTo(3));
 	}
 
-//		@Test
-	public void test3(){
+		@Test
+	public void assertPost(){
 
 		baseURI = "http://localhost:3000";
 
@@ -76,8 +69,8 @@ public class TestExamples {
 			statusCode(201).log().all();
 	}	
 
-//		@Test
-	public void test4(){
+		@Test
+	public void assertPut(){
 
 		baseURI = "http://localhost:3000";
 
@@ -97,8 +90,8 @@ public class TestExamples {
 			statusCode(200).log().all();
 	}	
 
-//	@Test
-	public void test5(){
+	@Test
+	public void deleteMethod(){
 
 		baseURI = "http://localhost:3000";
 
